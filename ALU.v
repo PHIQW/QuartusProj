5c8bf73223d2a5439ef7c2a4c2d84d508c811f46
Phillip Pham <43395993+PHIQW@users.noreply.github.com>
2020-01-30 13:34:08 -0500

    Completed and tested new ALU on DE2-115 board

--- ALU.v
+++ ALU.v
@@ -1,8 +1,7 @@
 `timescale 1ns / 1ns
 
-module part3(LEDR, SW,HEX0,HEX1,HEX2,HEX3,HEX4,HEX5,KEY);
-    input [9:0] SW;
-	 input [2:0] KEY;
+module part3(LEDR, SW,HEX0,HEX1,HEX2,HEX3,HEX4,HEX5);
+    input [17:0] SW;
 	 
     output [9:0] LEDR;
 	 output [7:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
@@ -13,7 +12,7 @@ module part3(LEDR, SW,HEX0,HEX1,HEX2,HEX3,HEX4,HEX5,KEY);
         .ALUout(wireALUout[7:0]),
         .A(SW[7:4]),
         .B(SW[3:0]),
-		  .key(KEY[2:0])
+		  .key(SW[17:15])
         );
 		  
 	 lightDisplays my_lights(
@@ -25,7 +24,8 @@ module part3(LEDR, SW,HEX0,HEX1,HEX2,HEX3,HEX4,HEX5,KEY);
 			.hex4(HEX4[7:0]),
 			.hex5(HEX5[7:0]),
 			.A(SW[7:4]),
-			.B(SW[3:0])
+			.B(SW[3:0]),
+			.ALUout(wireALUout)
 			);
 endmodule
 
@@ -42,6 +42,7 @@ module lightDisplays(outLEDs, hex0, hex1, hex2, hex3, hex4, hex5, A, B, ALUout);
 	hex_display hx3(.IN(4'b0000),.OUT(hex3));
 	hex_display hx4(.IN(ALUout[3:0]),.OUT(hex4));
 	hex_display hx5(.IN(ALUout[7:4]),.OUT(hex5));
+	
 endmodule
 
 //for processing the calculations of the inputs
@@ -65,18 +66,18 @@ module ALU(ALUout,A,B,key);
 								  .cin(1'b0)
 								  );							  
   //case 1: verilog + operator
-	assign wirec1 = {4'b000 , A+B};
+	assign wirec1 = A+B;
 	
 	//case 2: or upper, xor lower
 	assign wirec2 = {A|B, A^B};
 	
 	//case3: at least 1 value 1
-	assign wirec3 = {7'b0000000, andResult};
-	or(andResult,A,B);
+	assign wirec3 = |{A, B};
+	or(orResult,A[0],B[0],A[1],B[1],A[2],B[2],A[3],B[3]);
 	
 	//case4: all values 1
-	assign wirec4 = {7'b0000000, orResult};
-	and(orResult,A,B);
+	assign wirec4 = {7'b0000000, andResult};
+	and(andResult,A[0],B[0],A[1],B[1],A[2],B[2],A[3],B[3]);
 	
 	//case5: A upper B lower
 	assign wirec5 = {A, B};
@@ -86,10 +87,10 @@ module ALU(ALUout,A,B,key);
 		case(key)
 			3'b000: ALUout = wirec0;//ripple adder
 			3'b001: ALUout = wirec1;//verilog + operator
-			3'b011: ALUout = wirec2;//or upper xor lower
-			3'b100: ALUout = wirec3;//at least 1 value 1
-			3'b101: ALUout = wirec4;//All value 1
-			3'b110: ALUout = wirec5;//A upper, b lower
+			3'b010: ALUout = wirec2;//or upper xor lower
+			3'b011: ALUout = wirec3;//at least 1 value 1
+			3'b100: ALUout = wirec4;//All value 1
+			3'b101: ALUout = wirec5;//A upper, b lower
 			default: ALUout = 8'b00000000; //default 0
 		endcase
 	end
