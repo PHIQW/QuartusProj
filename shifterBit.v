3b06c422ad6767088a2f4e0286d214a1306d326a
Phillip Pham <43395993+PHIQW@users.noreply.github.com>
2020-02-11 15:56:07 -0500

    separated flipflop and shifterbit modules

--- shifterBit.v
+++ shifterBit.v
@@ -1,17 +1,3 @@
-module flipflop(q, d, clock, reset_n);//1 bit
-	input d;
-	input clock, reset_n;
-	output reg q;
-	
-	always @(posedge clock)	//triggered when clock rises
-	begin
-		if(reset_n == 1'b0)	//when reset_n is 0...
-			q <= 0;				//... q is set 0
-		else						//when reset_n not 0...
-			q <= d;				//... value d is passed to q
-	end
-endmodule
-
 module shifterBit(load_val, load_n, shift, in, out, clk, reset_n);
 	input load_val, load_n, shift, in, clk, reset_n;
 	output out;
